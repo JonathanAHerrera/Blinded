598b61c3a3d114f0896fe01634b59d614f03e540
Cindy Lam <cindy.lam191@gmail.com>
2025-04-10 17:34:00 -0500

    created puzzle 1 material

---
+++ blinded/app/chessBoardP1/page.js
@@ -0,0 +1,14 @@
+export default function ChessBoard() {
+    return (
+      <div style={{
+        backgroundImage: 'url("chessBoardP1.jpeg")',
+        //backgroundSize: 'cover',
+        backgroundSize: '90%', // try 90%, 85%, etc. for more zoom out
+        backgroundPosition: 'center',
+        height: '100vh',
+        width: '100vw',
+      }}>
+        {/* You can add content here later */}
+      </div>
+    );
+  }
