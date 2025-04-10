ffa30ab59f8b33c4c7864bf7f9f8a71c88bcbc3e
Cindy Lam <cindy.lam191@gmail.com>
2025-04-10 17:31:20 -0500

    added crime scene page

---
+++ blinded/app/crimeScene/page.js
@@ -0,0 +1,14 @@
+export default function CrimeScene() {
+    return (
+      <div style={{
+        backgroundImage: 'url("crimeScene.jpg")',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        height: '100vh',
+        width: '100vw',
+      }}>
+        {/* You can add content here later */}
+      </div>
+    );
+  }
+  
