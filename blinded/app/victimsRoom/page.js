0c3156bc6e5df17b91e19ff4ff9eb14aead47f2e
Cindy Lam <cindy.lam191@gmail.com>
2025-04-10 18:01:55 -0500

    added victims room page

---
+++ blinded/app/victimsRoom/page.js
@@ -0,0 +1,15 @@
+export default function VictimsRoom() {
+    return (
+      <div style={{
+        backgroundImage: 'url("VictimsRoom.jpg")',
+        backgroundSize: '70%',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center',
+        backgroundColor: 'black',
+        height: '100vh',
+        width: '100vw',
+      }}>
+        {/* You can add content here later */}
+      </div>
+    );
+  }
