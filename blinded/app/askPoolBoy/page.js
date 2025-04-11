faf111dd1dac5c0402044a8bcf7ecaff248766e3
Cindy Lam <cindy.lam191@gmail.com>
2025-04-11 13:29:28 -0500

    Added some puzzle 2 material

---
+++ blinded/app/askPoolBoy/page.js
@@ -0,0 +1,16 @@
+export default function askPoolBoy() {
+    return (
+      <div style={{
+        backgroundImage: 'url("askPoolBoy.jpeg")',
+        //backgroundSize: 'cover',
+        backgroundSize: '50%', // try 90%, 85%, etc. for more zoom out
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+        backgroundColor: 'black',
+        height: '100vh',
+        width: '100vw',
+      }}>
+        {/* You can add content here later */}
+      </div>
+    );
+  }
