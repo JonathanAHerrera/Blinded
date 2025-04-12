b845667ce16888c7adde70cb3491ecaea0ac2bfb
Cindy Lam <cindy.lam191@gmail.com>
2025-04-11 19:25:31 -0500

    added route from landing page to crime scene

--- blinded/app/page.js
+++ blinded/app/page.js
@@ -1,10 +1,19 @@
+"use client";
+
 import Image from "next/image";
 import styles from "./page.module.css";
 
+import { useRouter } from "next/navigation";
 
 
 
 export default function Home() {
+  const router = useRouter();
+
+  const handleStartClick = () => {
+    router.push("/crimeScene");
+  };
+
   return (
     <div style={{ backgroundImage: "url(login_background.png)", width: "100vw", height: "100vh", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
       <div className="centerBox" style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", gap:"40px"}}>
@@ -13,7 +22,7 @@ export default function Home() {
           <button style={{ backgroundColor: "transparent", border:"none" }}>
             <img src="volume_up.png"/>
           </button>
-          <button style={{ borderRadius: "10px", backgroundColor: "white", color: "black", border: "2px white solid", flex: "1", height: "100%", fontFamily: "var(--font-sue-ellen-francisco)", fontSize:"40px"}}>
+          <button onClick={handleStartClick} style={{ borderRadius: "10px", backgroundColor: "white", color: "black", border: "2px white solid", flex: "1", height: "100%", fontFamily: "var(--font-sue-ellen-francisco)", fontSize:"40px", cursor: "pointer"}}>
             Start
           </button>
           <button style={{ borderRadius: "10px", backgroundColor: "transparent", color: "white", border: "2px white solid", flex: "1", height: "100%", fontFamily: "var(--font-sue-ellen-francisco)", fontSize:"40px"}}>
