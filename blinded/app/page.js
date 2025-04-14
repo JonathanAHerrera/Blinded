e600bc0048c9dc595feb1549616f5083d4d648ac
Jonathan Herrera <Jonathan.a.herrera0@gmail.com>
2025-04-13 21:19:44 -0500

    Initial commit

--- blinded/app/page.js
+++ blinded/app/page.js
@@ -1,17 +1,21 @@
+"use client"
 import Image from "next/image";
 import styles from "./page.module.css";
-
-
-
+import { useState } from "react";
 
 export default function Home() {
+  const [ audio, setAudio ] = useState( true )
+
   return (
     <div style={{ backgroundImage: "url(login_background.png)", width: "100vw", height: "100vh", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
       <div className="centerBox" style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", gap:"40px"}}>
         <div style={{ color: "white", fontSize:"20vh", fontFamily: "var(--font-title)" }}> BLINDED </div>
         <div className="Buttons" style={{ width: "100%", minHeight: "100px", display: "flex", justifyContent: "space-between", alignItems: "center", gap:"20px"}}>
           <button style={{ backgroundColor: "transparent", border:"none" }}>
-            <img src="volume_up.png"/>
+            {
+              ( audio ) &&  <img src="volume_up.png"/>
+            
+            }
           </button>
           <button style={{ borderRadius: "10px", backgroundColor: "white", color: "black", border: "2px white solid", flex: "1", height: "100%", fontFamily: "var(--font-sue-ellen-francisco)", fontSize:"40px"}}>
             Start
