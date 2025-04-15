aa811ed34ff226a7400389276d5b8220a52d6ed5
Spencer Le <lespencer88@gmail.com>
2025-04-15 10:04:13 -0500

    Created poolBoyRoom2. We will have to figure out which room to use

---
+++ blinded/app/poolBoyRoom2/page.js
@@ -0,0 +1,105 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
+export default function PoolBoyRoom2() {
+    const router = useRouter();
+
+    const handleSafeBoxClick = () => {
+        router.push("/bookshelf");
+    };
+
+    const handleNoteClick = () => {
+        router.push("/note");
+    };
+
+    const handlePeriodicTableClick = () => {
+        router.push("/periodicTable");
+    };
+
+    const handleClosetClick = () => {
+        router.push("/closet");
+    };
+    
+    return (
+        <div style={{
+            backgroundImage: 'url("poolBoyRoom.png")',
+            backgroundSize: 'contain',
+            backgroundRepeat: 'no-repeat',
+            backgroundPosition: 'center',
+            backgroundColor: 'black',
+            height: '100vh',
+            width: '100vw',
+        }}>
+
+            <button
+                onClick={handleSafeBoxClick}
+                style={{
+                    position: "absolute",
+                    top: "50%",
+                    left: "32%",
+                    width: "40px",
+                    height: "40px",
+                    borderRadius: "50%",
+                    backgroundColor: "rgba(255, 255, 0, 0.05)",
+                    boxShadow: "0 0 0 transparent",
+                    transition: "box-shadow 0.3s ease",
+                    border: "none",
+                    cursor: "pointer",
+                }}
+            />
+
+            <button
+                onClick={handleNoteClick}
+                style={{
+                    position: "absolute",
+                    top: "66%",
+                    left: "41%",
+                    width: "40px",
+                    height: "40px",
+                    borderRadius: "50%",
+                    backgroundColor: "rgba(255, 255, 0, 0.05)",
+                    boxShadow: "0 0 0 transparent", 
+                    transition: "box-shadow 0.3s ease",
+                    border: "none",
+                    cursor: "pointer",
+                }}
+            />
+
+            <button
+                onClick={handlePeriodicTableClick}
+                style={{
+                    position: "absolute",
+                    top: "35%",
+                    left: "41%",    
+                    width: "40px",
+                    height: "40px",
+                    borderRadius: "50%",
+                    backgroundColor: "rgba(255, 255, 0, 0.05)",
+                    boxShadow: "0 0 0 transparent",
+                    transition: "box-shadow 0.3s ease",
+                    border: "none",
+                    cursor: "pointer",
+                }}
+            />
+
+            <button
+                onClick={handleClosetClick}
+                style={{
+                    position: "absolute",
+                    top: "45%",
+                    left: "57%",    
+                    width: "40px",
+                    height: "40px",
+                    borderRadius: "50%",
+                    backgroundColor: "rgba(255, 255, 0, 0.05)",
+                    boxShadow: "0 0 0 transparent",
+                    transition: "box-shadow 0.3s ease",
+                    border: "none",
+                    cursor: "pointer",
+                }}
+            />
+
+        </div>
+    )
+}   
